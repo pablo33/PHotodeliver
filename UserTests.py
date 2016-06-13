7327b52c2e99d83fff59ca761d59ae2d8664b5f7
pablo33 <pcasas33@gmail.com>
2016-06-13 23:50:43 +0200

    Working on Test2, user tests

--- UserTests.py
+++ UserTests.py
@@ -34,7 +34,7 @@ def FetchFileSet (path):
 	return fileset
 
 
-class single_input (unittest.TestCase):
+class TestPack1 (unittest.TestCase):
 	""" Single photo input and processing TestPack1 """
 
 	reftest = 'Test1'
@@ -53,8 +53,8 @@ class single_input (unittest.TestCase):
 
 		SetTestPack (self.reftest)
 		os.system ('python3 PhotodeliverII.py \
-			 -ol Test_examples_container/UserTests/Test1/originlocation \
-			 -dl Test_examples_container/UserTests/Test1/destlocation \
+			 -ol %(testfolder)s/originlocation \
+			 -dl %(testfolder)s/destlocation \
 			 -rm 0 \
 			 -rp 0 \
 			 -minp 0 \
@@ -68,7 +68,7 @@ class single_input (unittest.TestCase):
 			 -clean 0 \
 			 -sfm 0 \
 			 -conv 0 \
-			 '
+			 '%{'testfolder':self.testfolder}
 			 )
 
 		known_values = set ([
@@ -126,8 +126,8 @@ class single_input (unittest.TestCase):
 
 		SetTestPack (self.reftest)
 		os.system ('python3 PhotodeliverII.py \
-			 -ol Test_examples_container/UserTests/Test1/originlocation \
-			 -dl Test_examples_container/UserTests/Test1/destlocation \
+			 -ol %(testfolder)s/originlocation \
+			 -dl %(testfolder)s/destlocation \
 			 -rm 0 \
 			 -rp 0 \
 			 -minp 0 \
@@ -141,7 +141,7 @@ class single_input (unittest.TestCase):
 			 -clean 0 \
 			 -sfm 0 \
 			 -conv 0 \
-			 '
+			 '%{'testfolder':self.testfolder}
 			 )
 
 		known_values = set ([
@@ -188,14 +188,14 @@ class single_input (unittest.TestCase):
 			files in .Trash folder are processed
 			preserve albums is not active:
 				tested with 1 folder and picture at originlocation
-			consider destination files is active
+			consider destination files is active, but there is not enough files to be cosidered.
 
 			'''
 
 		SetTestPack (self.reftest)
 		os.system ('python3 PhotodeliverII.py \
-			 -ol Test_examples_container/UserTests/Test1/originlocation \
-			 -dl Test_examples_container/UserTests/Test1/destlocation \
+			 -ol %(testfolder)s/originlocation \
+			 -dl %(testfolder)s/destlocation \
 			 -rm 0 \
 			 -rp 0 \
 			 -minp 0 \
@@ -209,7 +209,7 @@ class single_input (unittest.TestCase):
 			 -clean 1 \
 			 -sfm 0 \
 			 -conv 0 \
-			 '
+			 '%{'testfolder':self.testfolder}
 			 )
 
 		known_values = set ([
@@ -240,6 +240,74 @@ class single_input (unittest.TestCase):
 			'Test_examples_container/UserTests/Test1/destlocation/2016/2016-06/picture from my inmutable album 2016-06-12 19-47-56.png',
 			'Test_examples_container/UserTests/Test1/destlocation/2016/2016-06/20160606_195355.jpg',
 			'Test_examples_container/UserTests/Test1/destlocation/2016/2016-06/Screenshot from 2016-06-07 23-45-47.png',
+			])
+
+		result = FetchFileSet (self.testfolder)
+		self.assertEqual(known_values, result)
+
+class TestPack2 (unittest.TestCase):
+	""" Multiple photo handling and processing, TestPack2 """
+
+	reftest = 'Test2'
+	testfolder = os.path.join (dyntestfolder,reftest)
+
+
+	def test_simplegroup (self):
+		''' Simple grouping in events, (5 minimun pictures in this test)
+			clear folders is active
+			consider destination files is active
+			'''
+
+		SetTestPack (self.reftest)
+		os.system ('python3 PhotodeliverII.py \
+			 -ol %(testfolder)s/originlocation \
+			 -dl %(testfolder)s/destlocation \
+			 -rm 0 \
+			 -rp 0 \
+			 -minp 5 \
+			 -gap 28800 \
+			 -cpmode m \
+			 -cdi 1 \
+			 -mef 0 \
+			 -it 1 \
+			 -pa 1 \
+			 -faff 0 \
+			 -clean 1 \
+			 -sfm 0 \
+			 -conv 0 \
+			 '%{'testfolder':self.testfolder}
+			 )
+
+		known_values = set ([
+			'Test_examples_container/UserTests/Test2/originlocation',
+			'Test_examples_container/UserTests/Test2/destlocation',
+			'Test_examples_container/UserTests/Test2/destlocation/2015',
+			'Test_examples_container/UserTests/Test2/destlocation/2015/2015-12',
+			'Test_examples_container/UserTests/Test2/destlocation/2015/2015-12/Screenshot from 2015-12-13 23-20-58.png',
+			'Test_examples_container/UserTests/Test2/destlocation/2015/2015-12/Screenshot from 2015-12-13 23-20-33.png',
+			'Test_examples_container/UserTests/Test2/destlocation/2015/2015-12/Screenshot from 2015-12-13 23-20-49.png',
+			'Test_examples_container/UserTests/Test2/destlocation/2016',
+			'Test_examples_container/UserTests/Test2/destlocation/2016/2016-06-13',
+			'Test_examples_container/UserTests/Test2/destlocation/2016/2016-06-13/Screenshot from 2016-06-13 23-17-06.png',
+			'Test_examples_container/UserTests/Test2/destlocation/2016/2016-06-13/Screenshot from 2016-06-13 23-17-20.png',
+			'Test_examples_container/UserTests/Test2/destlocation/2016/2016-06-13/Screenshot from 2016-06-13 23-17-13.png',
+			'Test_examples_container/UserTests/Test2/destlocation/2016/2016-06-13/Screenshot from 2016-06-13 23-17-26.png',
+			'Test_examples_container/UserTests/Test2/destlocation/2016/2016-06-13/Screenshot from 2016-06-13 23-16-48.png',
+			'Test_examples_container/UserTests/Test2/destlocation/2016/2016-06-13/Screenshot from 2016-06-13 23-17-00.png',
+			'Test_examples_container/UserTests/Test2/destlocation/2014',
+			'Test_examples_container/UserTests/Test2/destlocation/2014/2014-05-10',
+			'Test_examples_container/UserTests/Test2/destlocation/2014/2014-05-10/Screenshot from 2014-05-10_1.png',
+			'Test_examples_container/UserTests/Test2/destlocation/2014/2014-05-10/Screenshot from 2014-05-10_4.png',
+			'Test_examples_container/UserTests/Test2/destlocation/2014/2014-05-10/Screenshot from 2014-05-10_3.png',
+			'Test_examples_container/UserTests/Test2/destlocation/2014/2014-05-10/Screenshot from 2014-05-10_2.png',
+			'Test_examples_container/UserTests/Test2/destlocation/2014/2014-05-10/Screenshot from 2014-05-10_5.png',
+			'Test_examples_container/UserTests/Test2/destlocation/2003/2003-03-21/Screenshot from 2003-03-21_1.png',
+			'Test_examples_container/UserTests/Test2/destlocation/2003',
+			'Test_examples_container/UserTests/Test2/destlocation/2003/2003-03-21',
+			'Test_examples_container/UserTests/Test2/destlocation/2003/2003-03-21/Screenshot from 2003-03-21_2.png',
+			'Test_examples_container/UserTests/Test2/destlocation/2003/2003-03-21/Screenshot from 2003-03-21_3.png',
+			'Test_examples_container/UserTests/Test2/destlocation/2003/2003-03-21/Screenshot from 2003-03-21_4 already on dest folder.png',
+			'Test_examples_container/UserTests/Test2/destlocation/2003/2003-03-21/Screenshot from 2003-03-21_5 already on destination.png',
 			])
 
 		result = FetchFileSet (self.testfolder)
